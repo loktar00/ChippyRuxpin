535264fd1f0cf2193a81d30ed7d35f16f6b9684c
Jason <loktar69@hotmail.com>
2017-02-20 04:12:17 -0500

    Update chippyRuxpin.py
    Updated gpio offsets for newer chips

--- chippyRuxpin.py
+++ chippyRuxpin.py
@@ -30,10 +30,10 @@ from chippyRuxpin_webFramework import WebFramework
 
 fullMsg = ""
 
-MOUTH_OPEN = 408 # GPIO pin assigned to open the mouth. XIO-P0
-MOUTH_CLOSE = 412 # GPIO pin assigned to close the mouth. XIO-P2
-EYES_OPEN = 410 # GPIO pin assigned to open the eyes. XIO-P4
-EYES_CLOSE = 414 # GPIO pin assigned to close the eyes. XIO-P6
+MOUTH_OPEN = 1013 # GPIO pin assigned to open the mouth. XIO-P0
+MOUTH_CLOSE = 1015 # GPIO pin assigned to close the mouth. XIO-P2
+EYES_OPEN = 1019 # GPIO pin assigned to open the eyes. XIO-P4
+EYES_CLOSE = 1017 # GPIO pin assigned to close the eyes. XIO-P6
 
 io = GPIO() #Establish connection to our GPIO pins.
 io.setup( MOUTH_OPEN )
